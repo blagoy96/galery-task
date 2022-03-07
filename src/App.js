4b28cd918b0327211c6fd4628268fe2cdeb642b8
blagoy96 <blagoy.stoitsov@gmail.com>
2022-03-07 17:42:28 +0200

    config google login

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <Login />
-      <div>Hello</div>
     </>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,35 @@
 import GoogleLogin from "react-google-login";
 
 function Login() {
-  return <GoogleLogin />;
+  const customStyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    marginTop: "-50px",
+    marginLeft: "-100px",
+  };
+  const handleLogin = (googleData) => {
+    console.log(googleData);
+  };
+  const handleFailure = (result) => {
+    alert(result);
+  };
+  return (
+    <>
+      <GoogleLogin
+        render={(renderProps) => (
+          <div style={customStyle}>
+            <button onClick={renderProps.onClick}>login</button>
+          </div>
+        )}
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Log in with Google"
+        onSuccess={handleLogin}
+        onFailure={handleFailure}
+        cookiePravicy={"single_host_origin"}
+      ></GoogleLogin>
+    </>
+  );
 }
 
 export default Login;
