5953364ba6d34c614a43430c176d992809bc6c8a
blagoy96 <blagoy.stoitsov@gmail.com>
2022-03-11 17:15:55 +0200

    Initialize signOut button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import { useGoogleAuth } from "../Login/GoogleAuthProvider";
 import "./Header.css";
 function Header() {
-  const { isInitialized, googleUser } = useGoogleAuth();
+  const { isInitialized, googleUser, isSignedIn } = useGoogleAuth();
   const username = isInitialized
     ? googleUser?.profileObj?.givenName ?? "Unknown user"
     : "";
 
-  return <header className="loginName">{username}</header>;
+  return (
+    <header className="header">
+      <div className="loginName">{username}</div>
+      {isSignedIn ? (
+        <div className="signOut">
+          <button>Sign out</button>
+        </div>
+      ) : null}
+    </header>
+  );
 }
 
 export default Header;
